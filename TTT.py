90c744965259fb5eb5996e3d770e015cd00879dc
CS21B043 <110807261+CS21B043@users.noreply.github.com>
2024-03-08 08:31:06 +0530

    Changed check functions and fixed the queue error
    Instead of returning +1,-1,0 the check_gameover function is now designed to return the difference, which is the maximum absolute difference across all rows, columns, and diagonals...essentially the best chance to win and who has that chance(+,-). 
    
    There were 2 more errors: 
    1)The parent's is_max was directly assigned to the children, which is obviously wrong, we should invert it. Fixed the issue.
    2) Once a gameover state entered the queue, instead of pushing it over and continuing to expand the remaining states, the algorithm halted there. 
    This was because we popped from the queue only if it wasn't a end state, which is obviously wrong. We should pop it anyway regardless of whether its a leaf or not

--- TTT.py
+++ TTT.py
@@ -4,71 +4,151 @@ p1_symbol = 'X'
 p2_symbol = 'O'
 empty_symbol = 'e'
 
-#Assumption: RETURN +1 if p1 wins, -1 if p2 wins and 0 if no one has one yet
-# (based on this plus the number of e's in board we can figure out if its a draw)
+# def row_check_S(state):
+#     #ROW CHECK
+#     n= len(state)
+#     for i in range(n):
+#         p1_count = 0
+#         p2_count = 0
+#         for j in range(n):
+#             p1_count+=state[i][j]==p1_symbol
+#             p2_count+=state[i][j]==p2_symbol
+#         if p2_count==(n):
+#             return -1
+#         if p1_count==(n):
+#             return 1
+#     return 0
+    
+# def column_check_S(state):
+#     n = len(state)
+#     #COLUMN CHECK
+#     for i in range(n):
+#         p1_count = 0
+#         p2_count = 0
+#         for j in range(n):
+#             p1_count+=state[j][i]==p1_symbol
+#             p2_count+=state[j][i]==p2_symbol
+#         if p2_count==n:
+#             return -1
+#         if p1_count==n:
+#             return 1
+#     return 0
+
+# def diagonal_check_S(state):
+#     #Diagonal Check(we only need to check the 2 principal diagonals as everything else won't give a sum of n)
+#     n = len(state)
+#     p2_count = 0
+#     p1_count = 0
+#     for j in range(n):
+#             p1_count+=state[j][j]==p1_symbol
+#             p2_count+=state[j][j]==p2_symbol
+#     if p2_count==n:
+#         return -1
+#     if p1_count==n:
+#         return 1
+#     p1_count = 0
+#     p2_count = 0
+#     for j in range(n):
+#             p1_count+=state[-1-j][j]==p1_symbol
+#             p2_count+=state[-1-j][j]==p2_symbol
+#     if p2_count==n:
+#         return -1
+#     if p1_count==n:
+#         return 1
+#     return 0
+# def check_gameover_S(state):
+#     #ROW CHECK
+#     ret = row_check_S(state)
+#     if ret:
+#         return ret
+#     #Column Check
+#     ret = column_check_S(state)
+#     if ret:
+#         return ret
+#     #Diagonal Check
+#     return diagonal_check_S(state)
+#     #If none of the three are matched, we return 0 to indicate no one has won(yet)
+
+#Return the actual difference of the maximum absolute difference b/w p1_symbols and p2_symbols across all rows 
+#i.e, if a row has full p2_symbols maximum abs diff is n, but we must return -n to differentiate it from row of p1_symbols 
 def row_check_S(state):
     #ROW CHECK
     n= len(state)
+    diff = 0
     for i in range(n):
         p1_count = 0
         p2_count = 0
         for j in range(n):
             p1_count+=state[i][j]==p1_symbol
             p2_count+=state[i][j]==p2_symbol
-        if p2_count==(n):
-            return -1
-        if p1_count==(n):
-            return 1
-    return 0
+        if(abs(p1_count-p2_count) > abs(diff)):
+            diff = p1_count - p2_count
+        if abs(diff)==(n):
+            return diff
+    return diff
     
+
+#Return the actual diff of maximum absolute difference b/w p1_symbols and p2_symbols across all columns    
 def column_check_S(state):
     n = len(state)
     #COLUMN CHECK
+    diff = 0
     for i in range(n):
         p1_count = 0
         p2_count = 0
         for j in range(n):
             p1_count+=state[j][i]==p1_symbol
             p2_count+=state[j][i]==p2_symbol
-        if p2_count==n:
-            return -1
-        if p1_count==n:
-            return 1
-    return 0
+        if(abs(p1_count-p2_count) > abs(diff)):
+            diff = p1_count - p2_count
+        if abs(diff)==(n):
+            return diff
+    return diff
 
+
+#Return the actual diff of maximum absolute difference b/w p1_symbols and p2_symbols across 2 key diags
 def diagonal_check_S(state):
     #Diagonal Check(we only need to check the 2 principal diagonals as everything else won't give a sum of n)
     n = len(state)
     p2_count = 0
     p1_count = 0
+    diff = 0
     for j in range(n):
-            p1_count+=state[j][j]==p1_symbol
-            p2_count+=state[j][j]==p2_symbol
-    if p2_count==n:
-        return -1
-    if p1_count==n:
-        return 1
+        p1_count+=state[j][j]==p1_symbol
+        p2_count+=state[j][j]==p2_symbol
+    if(abs(p1_count-p2_count) > abs(diff)):
+        diff = p1_count - p2_count
+    if abs(diff)==(n):
+        return diff
     p1_count = 0
     p2_count = 0
     for j in range(n):
-            p1_count+=state[n-1-j][j]==p1_symbol
-            p2_count+=state[n-1-j][j]==p2_symbol
-    if p2_count==n:
-        return -1
-    if p1_count==n:
-        return 1
-    return 0
+        p1_count+=state[-1-j][j]==p1_symbol
+        p2_count+=state[-1-j][j]==p2_symbol
+    if(abs(p1_count-p2_count) > abs(diff)):
+        diff = p1_count - p2_count
+    if abs(diff)==(n):
+        return diff
+    return diff
+
+
+#Assumption: RETURN +1 if p1 wins, -1 if p2 wins and 0 if no one has one yet
+# (based on this plus the number of e's in board we can figure out if its a draw)
 def check_gameover_S(state):
+    n = len(state)
     #ROW CHECK
     ret = row_check_S(state)
-    if ret:
-        return ret
+    if abs(ret)==n:
+        return ret//abs(ret)
     #Column Check
     ret = column_check_S(state)
-    if ret:
-        return ret
+    if abs(ret)==n:
+        return ret//abs(ret)
     #Diagonal Check
-    return diagonal_check_S(state)
+    ret = diagonal_check_S(state)
+    if abs(ret)==n:
+        return ret//abs(ret)
+    return 0
     #If none of the three are matched, we return 0 to indicate no one has won(yet)
 
 def state_init_S(n):
@@ -80,6 +160,7 @@ def state_init_S(n):
         state.append(row)
     return state
 
+
 def next_states_S(pres_state, is_max):
     next_states=[]
     n=len(pres_state)
@@ -88,14 +169,14 @@ def next_states_S(pres_state, is_max):
         symbol=p1_symbol
     else:
         symbol=p2_symbol
-    
+    children_state = not is_max
     for i in range(n):
         for j in range(n):
             if pres_state[i][j]==empty_symbol:
                 pres_state[i][j]=symbol
                 # print(pres_state)
                 dup = copy.deepcopy(pres_state)
-                next_states.append(state_D(dup, is_max))
+                next_states.append(state_D(dup, children_state))
                 # print(" after: ", pres_state)
                 pres_state[i][j]=empty_symbol
     return next_states
@@ -116,25 +197,30 @@ class state_D():
 def build_tree_D(root,depth, is_max):
     n = len(root.state)
     q = [root]
-    while(depth < n*n):
+    keep_going = True
+    while(depth < n*n and keep_going):
         size=len(q)
-        print(size, is_max)
+        print(size, is_max, depth)
+        keep_going = False
         for i in range(size):
             node=q[0]
+            q.pop(0)
             # print("start " , node.state)
             if node.is_over==0:
-                children = next_states_S(node.state,is_max)
+                keep_going = True
+                children = next_states_S(node.state, is_max)
                 node.add_children(children)
                 for child in children:
-                    # print(child.state)
+                    # print(child.state, child.is_max)
                     q.append(child)
-                q.pop(0)
         depth+=1
         is_max = not is_max
     return root
 
 def minimax_S(root):
     if len(root.children)==0:
+        root.minimax = root.is_over
+        # print(root.state, root.is_over)
         # if(root.is_over==-1):
         #     print(root.is_over)
         return root.is_over
@@ -147,16 +233,18 @@ def minimax_S(root):
             mini = v
         if(maxi < v):
             maxi = v
-    if root.is_max:
+    if root.is_max ==True:
         root.minimax=maxi
+        # print(root.state, root.minimax, p1_symbol)
         return maxi
     root.minimax=mini
+    # print(root.state, root.minimax, p2_symbol)
     return mini
 
 
 if __name__=='__main__':
-    start = state_init_S(4)
-    # start = [['X', 'e', 'e'], ['e','e','e'],  ['e','e','e']]
+    # start = state_init_S(2)
+    start = [['e', 'e', 'e'], ['e','e','e'],  ['e','e','e']]
     s0 = state_D(start, True)
     root = build_tree_D(s0,depth=0, is_max=True)
     print(minimax_S(root))
